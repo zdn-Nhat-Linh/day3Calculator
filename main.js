aa51bf7367aae179f8d97afdda1df23cc9027882
zdn-Nhat-Linh <linh.nguyen@zodinet.tech>
2023-02-22 16:12:11 +0700

    done v1

--- main.js
+++ main.js
@@ -1,15 +1,60 @@
+// ----------------- Normal JS
 var result = document.getElementById("result");
 
-const insert = (char) => (result.value += char);
+// const insert = (char) => (result.value += char);
 
-const back = () => (result.value = result.value.slice(0, -1));
+// const back = () => (result.value = result.value.slice(0, -1));
 
-const clean = () => (result.value = "");
+// const clean = () => (result.value = "");
 
-function calculate() {
-	try {
-		result.value ? (result.value = eval(result.value)) : alert("Please enter a number");
-	} catch (e) {
-		alert("You have entered an invalid expression");
-	}
-}
+// function calculate() {
+// 	try {
+// 		result.value ? (result.value = eval(result.value)) : alert("Please enter a number");
+// 	} catch (e) {
+// 		alert("You have entered an invalid expression");
+// 	}
+// }
+
+// ----------------- Normal JS with switch
+// function operate(char) {
+// 	switch (char) {
+// 		case "back":
+// 			result.value = result.value.slice(0, -1);
+// 			break;
+// 		case "clean":
+// 			result.value = "";
+// 			break;
+// 		case "calculate":
+// 			try {
+// 				result.value ? (result.value = eval(result.value)) : (result.value = "0");
+// 			} catch (e) {
+// 				alert("You have entered an invalid expression");
+// 			}
+// 			break;
+// 		default:
+// 			result.value += char;
+// 	}
+// }
+
+// ----------------- OOP
+// class calculator {
+// 	constructor() {
+// 		this.result = document.getElementById("result");
+// 	}
+
+// 	insert = (char) => (this.result.value += char);
+
+// 	back = () => (this.result.value = this.result.value.slice(0, -1));
+
+// 	clean = () => (this.result.value = "");
+
+// 	calculate = () => {
+// 		try {
+// 			result.value ? (result.value = eval(result.value)) : (result.value = "0");
+// 		} catch (e) {
+// 			alert("You have entered an invalid expression");
+// 		}
+// 	};
+// }
+
+// const calc = new calculator();
---
+++ mainWithTS.js
@@ -0,0 +1,48 @@
+var inputElement = document.getElementById("result");
+function operate(char) {
+    if (!inputElement)
+        throw new Error("Element not found");
+    switch (char) {
+        case "back":
+            inputElement.value = inputElement.value.slice(0, -1);
+            break;
+        case "clean":
+            inputElement.value = "";
+            break;
+        case "calculate":
+            try {
+                inputElement.value
+                    ? (inputElement.value = eval(inputElement.value))
+                    : (inputElement.value = "0");
+            }
+            catch (e) {
+                alert("You have entered an invalid expression");
+            }
+            break;
+        default:
+            inputElement.value += char;
+    }
+}
+// class Calculator {
+// 	private result: HTMLInputElement;
+// 	constructor() {
+// 		this.result = document.getElementById("result") as HTMLInputElement;
+// 	}
+// 	insert = (char: string): void => {
+// 		this.result.value += char;
+// 	};
+// 	back = (): void => {
+// 		this.result.value = this.result.value.slice(0, -1);
+// 	};
+// 	clean = (): void => {
+// 		this.result.value = "";
+// 	};
+// 	calculate = (): void => {
+// 		try {
+// 			this.result.value ? (this.result.value = eval(this.result.value)) : (this.result.value = "0");
+// 		} catch (e) {
+// 			alert("You have entered an invalid expression");
+// 		}
+// 	};
+// }
+// const calc = new Calculator();
